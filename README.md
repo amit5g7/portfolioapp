4dc4d93c99ec80008b17682144c54e7eaa312663
Amit Gupta <amitguptag@gmail.com>
2023-05-16 02:07:57 +0530

    Added Desc, image and links

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # This is a Portfolio Website
 ## Under Development
 
-### Watch out this space (mark down language is md)
+### Watch out this space (mark d)
---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("John Doe")
+    content="""this is a text on John Doe
+    
+    """
+    st.info(content)
+
+content2="""Below you can find some of the apps i have built in python. 
+Feel free to contact me!
+
+"""
+st.write(content2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
